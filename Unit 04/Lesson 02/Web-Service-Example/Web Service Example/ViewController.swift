637c9e75cfba41c6f32c3b69f348c6a1a69bc891
gangelo <web.gma@gmail.com>
2016-09-25 20:10:53 -0400

    Remove AFNetworking

--- Unit 04/Lesson 02/Web-Service-Example/Web Service Example/ViewController.swift
+++ Unit 04/Lesson 02/Web-Service-Example/Web Service Example/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+typealias NSURLSessionDataTask = URLSessionDataTask
+
 class ViewController: UIViewController {
     
     // MARK: Properties
@@ -15,14 +17,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        callWeatherService()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func callWeatherService() {
+       
+    }
 }
 
